f86e9a9037f094869e0db4af436b17216540babd
Suman061295 <gsuman1995@gmail.com>
2025-08-08 20:00:46 +0530

    test 2

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ module "create_ec2_create" {
   pem_key_name  = "Suman"
   instance_tag  = "test-instance"
 }
+
+module "create_ec2_create" {
+  source = "./modules/ec2"
+  instance_type = "t2.micro"
+  pem_key_name  = "Suman"
+  instance_tag  = "test-instance-1"
+}
